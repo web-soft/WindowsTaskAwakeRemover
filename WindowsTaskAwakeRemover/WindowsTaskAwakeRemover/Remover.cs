fa420a0bd5da79db6e6aea9512c563d4a7ffaff3
mlopes <mickael.lopes@sfr.fr>
2016-04-13 17:29:55 +0200

    init

---
+++ WindowsTaskAwakeRemover/WindowsTaskAwakeRemover/Remover.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32.TaskScheduler;
+
+namespace WindowsTaskAwakeRemover
+{
+    public class Remover
+    {
+        private int _nbRemovedAwake =0;
+        public Remover()
+        {
+            using (TaskService ts = new TaskService())
+            {
+                var tasks = ts.FindAllTasks(t => t.Definition.Settings.WakeToRun).ToList();
+                foreach (var t in tasks)
+                {
+                    Console.WriteLine(t.Name);
+                    t.Definition.Settings.WakeToRun = false;
+                    t.RegisterChanges();
+                    _nbRemovedAwake++;
+                }
+            }
+            if (_nbRemovedAwake == 0)
+                Console.WriteLine("No schedule Tasks can awake you computer found");
+            else
+                Console.WriteLine(_nbRemovedAwake + "Schedule Tasks will not awake you computer anymore :)");
+        }
+    }
+}
